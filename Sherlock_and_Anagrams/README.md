652a1cafb8fee21197f418e399871d4170b45d20
Mayur Divate <mayur.divate91@gmail.com>
2021-07-25 19:54:44 +1000

    works well with sample data

---
+++ Sherlock_and_Anagrams/README.md
@@ -0,0 +1,33 @@
+
+Two strings are anagrams of each other if the letters of one string can be rearranged to form the other string. Given a string, find the number of pairs of substrings of the string that are anagrams of each other.
+
+### Example 
+s = mom 
+
+The list of all anagrammatic pairs is  [m,m], [mo,mo] at positions [[0],[2]] , [[0,1],[1,2]] respectively.
+
+Complete the function sherlockAndAnagrams.
+
+sherlockAndAnagrams has the following parameter(s):
+- string s: a string
+
+Returns
+- int: the number of unordered anagrammatic pairs of substrings in 
+
+
+Constraints
+0 < q < 11
+3 <  length of s < 101
+
+
+##### Sample Input 0
+```
+abba
+abcd
+```
+
+#### Sample Output 0
+```
+4
+0
+```
---
+++ Sherlock_and_Anagrams/sherlock_and_anagrams.py
@@ -0,0 +1,44 @@
+
+def sherlockAndAnagrams(s):
+
+    i = 0
+
+    # create dict of words
+    # words are created from overlapping substrings
+    # characters in the words are sorted before making it key
+    # words are keys and their counts in the string are values
+    anagramS = getOverlappingSubstrings(s)
+
+    for k in anagramS.keys():
+        if anagramS[k] > 1:
+            i += 1
+
+    return i
+
+
+
+
+def getOverlappingSubstrings(s):
+
+    dictx = {}
+
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            sub_string = s[i:j+1]
+            anagramS = ''.join(sorted(sub_string))
+
+            if anagramS in dictx.keys():
+                dictx[anagramS] += dictx[anagramS]
+
+            else:
+                dictx[anagramS] = 1
+
+
+    return dictx
+
+
+
+
+
+s = 'abba'
+sherlockAndAnagrams(s)
