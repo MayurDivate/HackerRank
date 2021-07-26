375676d2c44eb9d1ac988f053440511ca57602fe
Mayur Divate <mayur.divate91@gmail.com>
2021-07-26 10:23:37 +1000

    head markers removed

--- Sherlock_and_Anagrams/sherlock_and_anagrams.py
+++ Sherlock_and_Anagrams/sherlock_and_anagrams.py
@@ -11,18 +11,12 @@ def sherlockAndAnagrams(s):
     anagramS = getOverlappingSubstrings(s)
     #print(anagramS)
     for k in anagramS.keys():
-<<<<<<< HEAD
         # pairs =  half of (wordcount - 1 * wordcount)
         res += ((anagramS[k] - 1) * anagramS[k]) // 2
-<<<<<<< HEAD
         #print(anagramS[k])
         res += (anagramS[k]-1) * anagramS[k] // 2
-=======
-=======
         #print(anagramS[k])
         res += (anagramS[k]-1) * anagramS[k] // 2
->>>>>>> main
->>>>>>> substr
 
     return res
 
