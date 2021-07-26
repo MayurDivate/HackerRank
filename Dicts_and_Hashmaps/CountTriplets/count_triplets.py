d24c9c010a5e3d6caa27f3a9da83a610d21a156b
Mayur Divate <mayur.divate91@gmail.com>
2021-07-26 23:26:40 +1000

    successful on test data input06.txt and annotated code

--- Dicts_and_Hashmaps/CountTriplets/count_triplets.py
+++ Dicts_and_Hashmaps/CountTriplets/count_triplets.py
@@ -1,79 +1,44 @@
-# solution 1
+
 def countTriplets(arr, r):
-    count = 0
-    dict_freq = {}
-    dict_pairs = {}
 
-    for i in reversed(arr):
-        if i * r in dict_pairs:
-            count += dict_pairs[i * r]
-        if i * r in dict_freq:
-            dict_pairs[i] = dict_pairs.get(i, 0) + dict_freq[i * r]
-
-        dict_freq[i] = dict_freq.get(i, 0) + 1
-    print('Hacker',count)
-    print(dict_pairs)
-    return count
-
-# solution 2
-
-def countTripletsX(arr, r):
-    res = 0
+    ntriplets = 0
     num_freq = {}
     num_pair = {}
 
-    for num in reversed(arr):
-        num_r = num * r
-        num_r_r = num * r * r
+    for i in reversed(arr):
+        i_r = i * r
+        i_r_r = i * r * r
 
-        # num 1 if NUM 2 exist in pair means it's a triplet
-        res += num_pair.get((num_r, num_r_r), 0)
+        # if first number then increment triplet count
+        # by number of time pair of other two number exists
+        ntriplets += num_pair.get((i_r, i_r_r), 0)
 
-        # num 2 if num 3 exists
-        num_pair[(num, num_r)] = num_pair.get((num, num_r), 0) + num_freq.get(num_r, 0)
+        # if it is second number in the triplet
+        # i.e if only (i_r) present in the freq dict
+        # then add a pair to pair dict
+        # either it can be first entry pair value number times third num is present
+        # or zero
+        # as we are not checking presence of third number directly
+        # we will set it to zero
+        num_pair[(i, i_r)] = num_pair.get((i, i_r), 0) + num_freq.get(i_r, 0)
 
-        # for any number update the frequency
-        num_freq[num] = num_freq.get(num, 0) + 1
+        # if third element then add it to the freq dict
+        num_freq[i] = num_freq.get(i, 0) + 1
 
-
-    print('Solved',res)
-    return res
-
-
-
-#--------- test data
-
-#arr = [1, 2, 2, 4] # 2
-#r = 2
-
-#arr = [1, 3, 9, 9, 27, 81] # 6
-#r = 3
-
-#arr = [1, 5 ,5 ,25 ,125] # 4
-#r = 5
-
-arr = [5, 20, 25, 100, 125, 500]
-r = 5
-
-""""
-#print(arr)
-countTriplets(arr, r)
-countTripletsX(arr, r)
-
-"""
+    return ntriplets
 
 
-#input_06 expected output = 2325652489
-#                           1359210564
-#                           13621903916
-#r = 3
+#--------- test datasets ---------------
+filex = None
+#arr, r, out = [1, 2, 2, 4], 2, 2
+arr, r, out = [1, 3, 9, 9, 27, 81], 3, 6
+#arr, r, out = [1, 5 ,5 ,25 ,125] , 5, 4
+#arr, r, out = [5, 20, 25, 100, 125, 500], 5, 2
+#arr, filex, r , out = [], 'input06.txt', 100, 0
+#arr, filex, r, out  = [], 'input06.txt', 3, 2325652489
 
+if len(arr) == 0:
+    with open(filex) as f:
+        x = [arr.append(int(n)) for n in f.readline().split(' ')]
 
-# input_08 r = 100 output = 0
-r = 3
-with open('input06.txt') as f:
-    arr = [ int(n) for n in f.readline().split(' ')]
-    print(len(arr))
-    countTriplets(arr, r)
-    countTripletsX(arr, r)
-#"""
+print(countTriplets(arr, r), True if out == countTriplets(arr, r) else False)
