1d25de24d99042c5431adb31d4603a0976eee154
Mayur Divate <mayur.divate91@gmail.com>
2021-07-26 16:49:05 +1000

    failed on input 06 and timed out on few others

--- Dicts_and_Hashmaps/CountTriplets/count_triplets.py
+++ Dicts_and_Hashmaps/CountTriplets/count_triplets.py
@@ -3,13 +3,19 @@ def countTriplets(arr, r):
     res = 0
 
     num_counts = getNumCounts(arr)
-
+    #print(len(num_counts))
     for num in num_counts:
         # create expected triplet
         triplet = {num, num * r, num * (r ** 2)}
+        #print(num, triplet)
         if len(set(triplet) - num_counts.keys()) == 0:
-            res +=  max([num_counts.get(x) for x in triplet])
+            x = [num_counts.get(x) for x in triplet]
+            #print(x[0], x[1], x[2])
+            res += x[0] * x[1] * x[2]
 
+            #print([num_counts.get(x) for x in triplet])
+
+    print(res)
     return res
 
 
@@ -21,20 +27,25 @@ def getNumCounts(arr):
 
     return dictX
 
-"""
-arr = [1, 2, 2, 4]
-r = 2
 
-arr = [1, 3, 9, 9, 27, 81]
-r = 3
+#arr = [1, 2, 2, 4] # 2
+#r = 2
 
-arr = [1, 5 ,5 ,25 ,125]
-r = 5
-"""
+#arr = [1, 3, 9, 9, 27, 81] # 6
+#r = 3
+
+#arr = [1, 5 ,5 ,25 ,125] # 4
+#r = 5
+#countTriplets(arr, r)
+
+#"""
 
 
 #input_06 expected output = 2325652489
+#                           13621903916
 r = 3
 with open('input06.txt') as f:
-    arr = f.readline().split(' ')
+    arr = [ int(n) for n in f.readline().split(' ')]
+    print(len(arr))
     print(countTriplets(arr, r))
+#"""
