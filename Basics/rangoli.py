7cd30dab0e5690bd486d15d617242d4a67fb6fc2
Mayur Divate <mayur.divate91@gmail.com>
2021-08-04 17:17:05 +1000

    print upper triangle

--- Basics/rangoli.py
+++ Basics/rangoli.py
@@ -4,21 +4,28 @@ def print_rangoli(size):
     side += side - 1
     mid_point = (side + 1) / 2
 
+    start_point = mid_point
+    end_point = mid_point
     print(size, side, mid_point)
+
 
     for i in range(1, int(mid_point)+1):
         print()
         next_letter = size-1
         for j in range(1, side+1):
-           if (j % 2) != 0:
+           if ((j % 2) != 0) and (j >= start_point) and (j <= end_point):
                #print(abs(next_letter), end='')
                print(chr(ord('a')+abs(next_letter)), end='')
                next_letter -= 1
 
            else:
                print('-', end='')
-
-
+        if i < mid_point:
+            start_point -= 2
+            end_point += 2
+        elif i >= mid_point:
+            start_point += 2
+            end_point -= 2
 
 if __name__ == '__main__':
     n = int(input())
