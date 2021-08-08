b364686b54fd26864d4a3b44886268ba345e7ad2
Mayur Divate <mayur.divate91@gmail.com>
2021-08-08 15:01:27 +1000

    Test case 11 failed due time limit which was previously sucessfull

--- Dicts_and_Hashmaps/FrequencyQueries/freq_query.py
+++ Dicts_and_Hashmaps/FrequencyQueries/freq_query.py
@@ -2,24 +2,22 @@
 def freqQuery(queries):
 
     # int to freq mapping using dir
-    data = {}
+    freq_dict = {}
     res = []
 
     for action, element in queries:
-        # if element exist then perform insert / delete action
-        if (action in [1, 2]) and (element in data.keys()):
-            if action == 1:
-                data[element] += 1
-            elif action == 2:
-                data[element] -= 1
 
-        # or insert new element
-        elif action == 1:
-            data[element] = 1
+        # insert 1
+        if action == 1:
+            freq_dict[element] = freq_dict.get(element, 0) + 1
 
-        # or print if element with same frequency exists
+        # delete 2
+        elif action == 2:
+            freq_dict[element] = max(0, freq_dict.get(element, 0) - 1)
+
+        # print if element with same frequency exists
         elif action == 3:
-            if element in set(data.values()):
+            if element in set(freq_dict.values()):
                 res.append(1)
             else:
                 res.append(0)
@@ -28,5 +26,10 @@ def freqQuery(queries):
 
 
 #inputx = [(1,1),(2,2),(3,3),(1,1),(1,1),(2,1),(3,2)] # [0, 1]
-inputx = [(1, 3),(2, 3),(3, 2),(1, 4),(1, 5),(1, 5),(1, 4),(3, 2),(2, 4),(3, 2)] # [0, 1, 1 ]
-print(freqQuery(inputx))
+#inputx = [(1, 3),(2, 3),(3, 2),(1, 4),(1, 5),(1, 5),(1, 4),(3, 2),(2, 4),(3, 2)] # [0, 1, 1 ]
+
+inputx = [( int(x.rstrip().split(' ')[0]), int(x.rstrip().split(' ')[1])) for x in open('test12.csv')]
+
+out = freqQuery(inputx)
+print(len(out))
+print([i for i, e in enumerate(out) if e == 1])
