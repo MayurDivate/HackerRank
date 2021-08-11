71c4da26c5855bec1b269985648d0900e65deb6c
Mayur Divate <mayur.divate91@gmail.com>
2021-08-11 22:56:51 +1000

    Test data works fine

--- Recursion/CrosswordPuzzle/crossword_puzzle.py
+++ Recursion/CrosswordPuzzle/crossword_puzzle.py
@@ -45,7 +45,6 @@ class CrossWordPuzzel:
             if re.search(r'\-+', row):
                 blank_space = re.search(r'\-+', row).span()
                 blank_space = blank_space[1] - blank_space[0]
-                print('---w', row)
                 if blank_space == word_len:
                     self.crossword[i] = re.sub(r'\-+', word, row)
                     return self.crossword
@@ -53,30 +52,32 @@ class CrossWordPuzzel:
                     self.fill_vertical(word)
                     return self.crossword
 
-    def fill_horizontal(self, crossword, word):
-        pass
-
     def fill_horizontal_sub(self, word):
-        if word == 'ANKARA':
-            print(word)
         for i, row in enumerate(self.crossword):
-            if re.search(r'\-+\w+', row):
+            if re.search(r'\-+\w\-+', row):
+                m = re.search(r'(\-+)(\w+)(\-+)', row).groups()
+                start = len(m[0])
+                end = start + len(m[1])
+                wordx = word[start:end]
+                regx = '\w{' + str(len(m[0])) + '}' + m[1] + '\w{' + str(len(m[2])) + '}$'
+
+                if (wordx == m[1]) and (re.match(regx, word)):
+                    self.crossword[i] = re.sub(r'\-+\w\-+', word, row)
+
+            elif re.search(r'\-+\w+', row):
 
                 substr = re.search(r'(\-+)(\w+)', row).groups()
                 word = word.replace(substr[1], '')
 
                 if len(substr[0]) == len(word):
-                    print('replacement', self.crossword[i], word)
                     self.crossword[i] = re.sub(r'\-+', word, row)
                     return True
 
             elif re.search(r'\w+\-+', row):
-
                 substr = re.search(r'(\w+)(\-+)', row).groups()
                 word = word.replace(substr[0], '')
 
                 if len(substr[1]) == len(word):
-                    print('replacement', self.crossword[i], word)
                     self.crossword[i] = re.sub(r'\-+', word, row)
                     return True
 
@@ -88,27 +89,22 @@ class CrossWordPuzzel:
 
         # one word at time to fill
         for word in self.words:
-            print(' ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ')
-
             # if word is overlaps with previous then
             # try both vertical or horizontal.
             if self.is_overlap(word, old_word):
-                print(word, 'Yes overlapped')
 
                 if self.fill_horizontal_sub(word):
                     old_word = word
                 else:
-                    print('Here')
                     self.fill_vertical_sub(word)
                     old_word = word
-
-                print(*self.crossword, sep='\n')
 
             else:
 
                 self.fill_word(word)
                 old_word = word
-                print(*self.crossword, sep='\n')
+
+        print(*self.crossword, sep='\n')
 
 
 grid = ['+-++++++++',
