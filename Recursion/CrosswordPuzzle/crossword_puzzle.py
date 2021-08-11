bec4e4e7efb090e2a9c100557e04874bc43f002c
Mayur Divate <mayur.divate91@gmail.com>
2021-08-11 21:12:19 +1000

    converted to object oriented py program. Need to implment vertical filling with overlap method

--- Recursion/CrosswordPuzzle/crossword_puzzle.py
+++ Recursion/CrosswordPuzzle/crossword_puzzle.py
@@ -1,77 +1,96 @@
 import re
 
+class CrossWordPuzzel:
 
-def fill_vertical(crossword, word):
-    for i, letter in enumerate(word):
-        crossword[i] = re.sub(r'-',letter, crossword[i],1)
-    return crossword
+    def __init__(self, crossword, words):
+        self.crossword = crossword
+        self.words = words
+
+    def is_overlap(self, new_word, old_word):
+        # check if there was orverlap with previous word
+        if len(set(old_word) - set(new_word)) < len(old_word):
+            return True
+        return False
+
+    def fill_vertical(self, word):
+        # fill words vertically without interruption
+        for i, letter in enumerate(word):
+            self.crossword[i] = re.sub(r'-',letter, self.crossword[i],1)
+
+    def fill_vertical_sub(self, word):
+        pass
+
+    def fill_word(self, word):
+        word_len = len(word)
+
+        # grow through each row of grid
+        for i, row in enumerate(self.crossword):
+
+            # if grid contains blank space and
+            # it is less than size of word then
+            # fill it vertically
+
+            if re.search(r'\-+', row):
+                blank_space = re.search(r'\-+', row).span()
+                blank_space = blank_space[1] - blank_space[0]
+                if blank_space == word_len:
+                    self.crossword[i] = re.sub(r'\-+', word, row)
+                    return self.crossword
+                else:
+                    self.fill_vertical(word)
+                    return self.crossword
+
+    def fill_horizontal(self, crossword, word):
+        pass
+
+    def fill_horizontal_sub(self, word):
+
+        for i, row in enumerate(self.crossword):
+            if re.search(r'\-+\w+', row):
+
+                substr = re.search(r'(\-+)(\w+)', row).groups()
+                word = word.replace(substr[1], '')
+                if len(substr[0]) == len(word):
+                    x = re.sub(r'\-+', self.crossword[i], word)
+                    print(x)
+
+                self.fill_word(self.crossword, word)
+
+            elif re.search(r'\w+\-+', row):
+                substr = re.search(r'(\w+)(\-+)', row).groups()
+                word = word.replace(substr[0], '')
+
+                if len(substr[1]) == len(word):
+                    print(self.crossword[i], word)
+                    self.crossword[i] = re.sub(r'\-+', word, row)
+                    return self.crossword
 
 
-def is_overlap(new, old):
-    if len(set(old) - set(new)) < len(old):
-        return True
-    return False
+    def fillCrosswordPuzzle(self):
+        # set old not to nothing
+        old_word = ''
 
+        # one word at time to fill
+        for word in self.words:
 
-def fill_word(crossword, word):
-    word_len = len(word)
+            # if word is overlaps with previous then
+            # try both vertical or horizontal.
+            if self.is_overlap(word, old_word):
 
-    # grow through each row of grid
-    for i, row in enumerate(crossword):
+                if self.fill_horizontal_sub(word):
+                    old_word = word
+                    pass
+                else:
+                    self.fill_vertical_sub(word)
+                    old_word = word
 
-        # if grid contains blank space and
-        # it is less than size of word then
-        # fill it vertically
+                print(*self.crossword, sep='\n')
 
-        if re.search(r'\-+', row):
-            blank_space = re.search(r'\-+',row).span()
-            blank_space = blank_space[1] - blank_space[0]
-            if blank_space == word_len:
-                crossword[i] = re.sub(r'\-+', word, row)
-                return crossword
             else:
-                fill_vertical(crossword, word)
-                return crossword
 
-def fill_horizontal(crossword, word):
-    pass
-
-def fill_horizontal_sub(crossword, word):
-
-    for i, row in enumerate(crossword):
-        if re.search(r'\-+\w+',row):
-
-            substr = re.search(r'(\-+)(\w+)', row).groups()
-            word = word.replace(substr[1], '')
-            if len(substr[0]) == len(word):
-                x = re.sub(r'\-+', crossword[i], word)
-                print(x)
-
-            fill_word(crossword, word)
-
-        elif re.search(r'\w+\-+',row):
-            substr = re.search(r'(\w+)(\-+)', row).groups()
-            word = word.replace(substr[0],'')
-
-            if len(substr[1]) == len(word):
-                print(crossword[i], word)
-                crossword[i] = re.sub(r'\-+', word, row)
-                return crossword
-
-
-def crosswordPuzzle(crossword, words):
-    old_word = ''
-    for word in words:
-        if is_overlap(word, old_word):
-            print('Yes overlap ')
-            fill_horizontal_sub(crossword, word)
-
-        fill_word(crossword, word)
-        old_word = word
-        print(*crossword, sep='\n')
-
-
-
+                self.fill_word(word)
+                old_word = word
+                print(*self.crossword, sep='\n')
 
 
 grid = ['+-++++++++',
@@ -86,5 +105,7 @@ grid = ['+-++++++++',
         '+++++-++++']
 answers = 'LONDON;DELHI;ICELAND;ANKARA'.split(';')
 
-crosswordPuzzle(grid, answers)
+cw = CrossWordPuzzel(grid, answers)
+cw.fillCrosswordPuzzle()
+
 #fill_word(['+-----++++'], 'mayur')
